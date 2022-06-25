00d63c0463f2b7b3773a3b79fe3b5c8fb4de2d9f
mins1031 <urisegea@naver.com>
2022-06-26 00:31:44 +0900

    feat: delete 구현 완료

--- src/test/java/com/example/pointapi/event/review/ReviewEventTest.java
+++ src/test/java/com/example/pointapi/event/review/ReviewEventTest.java
@@ -51,7 +51,7 @@ class ReviewEventTest {
     //각 엑션 동작 테스트, 액션 조건 포인트 적용 테스트, 액션 예외, 공통 예외 테스트,
     @DisplayName("ADD 액션. content, photos, 첫리뷰 모두 적용, 포인트 3점 적립된다")
     @Test
-    void ADD_포인트3점() {
+    void ADD_3포인트() {
         //given
         User user = UserHelper.회원_생성(userRepository, "회원");
         Place place = PlaceHelper.장소_생성(placeRepository, "판교");
@@ -84,8 +84,7 @@ class ReviewEventTest {
         Assertions.assertThat(pointRecords.get(0).getCurrentPointAfterUpdate()).isEqualTo(3);
     }
 
-    //각 엑션 동작 테스트, 액션 조건 포인트 적용 테스트, 액션 예외, 공통 예외 테스트,
-    @DisplayName("ADD 액션. content, photos, 첫리뷰 모두 적용, 포인트 3점 적립된다")
+    @DisplayName("MODIFY 액션. 사진이 없었다가 추가되어 1포인트 적립된다")
     @Test
     void MODIFY_사진추가() {
         //given
@@ -124,7 +123,47 @@ class ReviewEventTest {
         Assertions.assertThat(pointRecords).hasSize(1);
         Assertions.assertThat(pointRecords.get(0).getUpdatedPoint()).isEqualTo(1);
         Assertions.assertThat(pointRecords.get(0).getCurrentPointAfterUpdate()).isEqualTo(2);
+    }
 
+    @DisplayName("DELETE 액션. content, photos, 첫리뷰 모두 적용된 상태에서 삭제되어 포인트 3점이 감소된다")
+    @Test
+    void DELETE_3포인트() {
+        //given
+        User user = UserHelper.회원_생성(userRepository, "회원");
+        Place place = PlaceHelper.장소_생성(placeRepository, "판교");
+        Review review = ReviewHelper.리뷰_생성(reviewRepository, user, place);
+        List<ReviewPhoto> reviewPhotos = reviewPhotoRepository.saveAll(Arrays.asList(
+                ReviewPhoto.createReviewPhoto("path1", review),
+                ReviewPhoto.createReviewPhoto("path2", review))
+        );
+
+        user.getPoint().updatePresentPoint(3);
+        user.getPoint().getReviewConditionChecker().changeContentPointState(true);
+        user.getPoint().getReviewConditionChecker().changePhotoPointState(true);
+        user.getPoint().getReviewConditionChecker().changePlacePointState(true);
+        userRepository.save(user);
+
+        EventOccurRequest eventOccurRequest = new EventOccurRequest(
+                EventType.REVIEW.getDesc(),
+                ReviewAction.DELETE.getDesc(),
+                review.getUuidIdentifier(),
+                null,
+                null,
+                user.getUuidIdentifier(),
+                place.getUuidIdentifier()
+        );
+
+        //when
+        reviewEvent.handlePoint(eventOccurRequest);
+
+        //then
+        User reviewer = userRepository.findById(user.getNum()).get();
+        Assertions.assertThat(reviewer.getPoint().getPresentPoint()).isEqualTo(0);
+
+        List<PointRecord> pointRecords = pointRecordRepository.findAll();
+        Assertions.assertThat(pointRecords).hasSize(1);
+        Assertions.assertThat(pointRecords.get(0).getUpdatedPoint()).isEqualTo(-3);
+        Assertions.assertThat(pointRecords.get(0).getCurrentPointAfterUpdate()).isEqualTo(0);
     }
 
 }
