12263db925b96dbaccbf985a64da6df8881956e4
mins1031 <urisegea@naver.com>
2022-06-25 22:09:48 +0900

    feat: 포인트 증가시 어떤 조건을 만족했는지 체크하는 클래스 구현 및 포인트 증감 이력 저장 로직 추가.

--- src/test/java/com/example/pointapi/common/PlaceHelper.java
+++ src/test/java/com/example/pointapi/common/PlaceHelper.java
@@ -2,9 +2,6 @@ package com.example.pointapi.common;
 
 import com.example.pointapi.place.domain.Place;
 import com.example.pointapi.place.repository.PlaceRepository;
-import com.example.pointapi.user.domain.Point;
-import com.example.pointapi.user.domain.User;
-import com.example.pointapi.user.repository.UserRepository;
 
 public class PlaceHelper {
 
